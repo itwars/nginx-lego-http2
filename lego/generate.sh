d42c3c9baf94d31cc09e18c4ab6700a0b88d51fc
Vincent RABAH <vincent.rabah@gmail.com>
2017-08-20 16:20:46 +0200

    some changes

--- lego/generate.sh
+++ lego/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker run --rm -it -p 1080:80 -p 1443:443 -v $(pwd)/.lego:/.lego \
+docker run --rm -it -p 80:80 -p 443:443 -v $(pwd)/.lego:/.lego \
 	-v /etc/localtime:/etc/localtime:ro \
-	itwars/lego --http=:80 --tls=:443 --email="vincent.rabah@gmail.com" --domains="api.it-wars.com" --key-type="rsa4096" --accept-tos run 
+	itwars/lego --http=:80 --tls=:443 --email="vincent.rabah@gmail.com" --domains="www.it-wars.com" --key-type="rsa4096" --accept-tos run 
 
--- nginx-http2/run.sh
+++ nginx-http2/run.sh
@@ -2,6 +2,6 @@ docker run -d \
 	-v /etc/localtime:/etc/localtime:ro \
 	-v ~/docker/myrepo/stack1/lego/.lego/certificates:/etc/certs \
 	-v ~/docker/myrepo/stack1/nginx-http2/www:/var/www \
-	-p 80:80 -p 443:443 \
+	-p 8080:80 -p 4443:443 \
 	itwars/nginx-http2
 
